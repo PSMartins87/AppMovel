f01bb0706e3cafbd6c567c15cc03fa8d6989c040
Paulo Silvério <paulosilveriomartinssilva11@gmail.com.br>
2024-02-28 19:05:21 -0300

    up

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { IonicModule } from '@ionic/angular';
+import { LoadingRestaurantComponent } from './loading-restaurant/loading-restaurant.component';
+import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { LoadingAccountComponent } from './loading-account/loading-account.component';
+
+@NgModule({
+    declarations: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent, OrderListComponent, LoadingAccountComponent],
+    imports: [
+        CommonModule,
+        IonicModule
+    ], exports: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent, OrderListComponent, LoadingAccountComponent]
+})
+export class ComponentsModule { }
